490fac4878f9bae9cdad82a2cce33ecc4b9374e3
khoiblack <ctdkhoi10042005.nvtroi2023@gmail.com>
2025-11-11 22:20:46 +0700

    complete

---
+++ ViDu_RecyclerView/app/src/main/java/ntu/khoi/vidu_recyclerview/LandScape.java
@@ -0,0 +1,28 @@
+package ntu.khoi.vidu_recyclerview;
+
+public class LandScape {
+
+    private String landImageFileName;
+    private String landCation;
+
+    public LandScape(String landImageFileName, String landCation) {
+        this.landImageFileName = landImageFileName;
+        this.landCation = landCation;
+    }
+
+    public String getLandImageFileName() {
+        return landImageFileName;
+    }
+
+    public void setLandImageFileName(String landImageFileName) {
+        this.landImageFileName = landImageFileName;
+    }
+
+    public String getLandCation() {
+        return landCation;
+    }
+
+    public void setLandCation(String landCation) {
+        this.landCation = landCation;
+    }
+}
